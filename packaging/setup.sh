eb7197f039aa8b2749d821e7369bb9e899f151a7
Cyril Rohr <cyril.rohr@gmail.com>
2017-07-20 18:17:36 +0200

    remove debug

--- packaging/setup.sh
+++ packaging/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "env"
-env
-
 # Not everyone chooses to run discourse behind Apache or Nginx.
 cat >> config/environments/production.rb <<EOF
 Discourse::Application.configure do
